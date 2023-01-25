8c56ec39ef1f21fe8541e99f07289dbf40128d52
E Luckie <eluckie.d@gmail.com>
2023-01-25 12:32:00 -0700

    add basic game format to index.html and create Player1 and Player2 objects
    Co-authored-by: Eliot Gronstal <eliot.lauren@gmail.com>
    Co-authored-by: Mesha Devan <madsketchz@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 // BUSINESS LOGIC
 
+// Business Logic For Player Objects:
+function PlayerOne(turnScore,runningScore) {
+  this.turnScore = turnScore;
+  this.runningScore = runningScore;
+}
 
-
-
+function PlayerTwo(turnScore,runningScore) {
+  this.turnScore = turnScore;
+  this.runningScore = runningScore;
+}
 
 
 
 // USER INTERFACE LOGIC
 
 window.addEventListener("load", function() {
-  
-}
+
+})
